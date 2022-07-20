ec71fd170d1c3c880005b279ad483ee2a59ae58c
lillielove <lillielove@gmail.com>
2022-07-20 15:30:32 -0700

    rewrite sql commands for app.get routes so that it incorporates party affiliation from new table, parties

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+-- must be deleted in this order because candidates references parties
+
 CREATE TABLE parties(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -8,5 +12,10 @@ CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+    -- because ^ references the parties table, it MUST exist before that line, i.e. why parties is created first
+    -- because it references parties(id), says that u cannot be member of party that doesnt exist (has no ID for that)
+    -- also says if a party is deleted, then changes party for candidate table to NULL
 );
