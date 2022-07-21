89a86a86dacfd28573981a79f8e78bbde41ce24d
lillielove <lillielove@gmail.com>
2022-07-20 17:38:23 -0700

    Added voters table to schema, seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS voters;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 -- must be deleted in this order because candidates references parties
@@ -19,3 +20,12 @@ CREATE TABLE candidates (
     -- because it references parties(id), says that u cannot be member of party that doesnt exist (has no ID for that)
     -- also says if a party is deleted, then changes party for candidate table to NULL
 );
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    -- DEFAULT is like a halfway to NOT NULL, where if you include no specifics, it will default to ^
+); 
